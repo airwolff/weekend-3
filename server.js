eb8ed3ed728aa0c3d507bae9a42ffcf5f22d69d1
Andy Wolff <andy.wolff@gmail.com>
2016-11-18 11:48:12 -0600

    works

--- server.js
+++ server.js
@@ -2,29 +2,21 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
-var numRouter = require('./routes/numbers');
+var numbers = require('./routes/numbers');
 
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
-app.use(express.static('public'));
-
-app.use('/numbers', numRouter);
-
-app.use(function (req, res, next) {
-	console.log('Got a request. yay!');
-	next();
-});
+app.use('/numbers', numbers);
 
 app.get('/', function (req, res) {
-	var filename = path.join(__dirname, 'public/views/index.html');
-	console.log('filename:', filename);
-	res.sendFile(filename);
+  res.sendFile(path.resolve('./public/index.html'));
 });
 
-app.set("port", (process.env.PORT || 3000));
+app.use(express.static('./public'));
 
-console.log("listening on 3000");
-
-app.listen(3000);
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function () {
+  console.log('Listening on port ', app.get('port'));
+});
